742f59a30d8c354f8de431cc938774627fcc7da5
alopgau <alopezg@ieslabahia.es>
2026-04-07 23:46:33 +0200

    logprocessor

--- src/main/kotlin/LogProcessor.kt
+++ src/main/kotlin/LogProcessor.kt
@@ -1,4 +1,16 @@
 package org.iesra
 
 class LogProcessor {
+    fun process(logs: Array<String>) {
+    logs.map {
+        log ->
+        val bracketsInfo = log.substringBefore("]").split(" ")
+        val afterBracketsInfo = log.substringAfter("]").split(" ")
+        val day = bracketsInfo[0]
+        val date = bracketsInfo[1]
+        val status = afterBracketsInfo[0]
+        val message = afterBracketsInfo.joinToString( " ")
+        log(day,date,status,message)
+    }
+    }
 }
