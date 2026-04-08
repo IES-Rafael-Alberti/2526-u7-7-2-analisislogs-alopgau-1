a4f09586086d5d7fe73bad9005e4748f54b902dc
alopgau <alopezg@iesrafaelalberti.es>
2026-04-08 13:18:17 +0200

    Método process terminado, enum log level y  clase log añadidas

---
+++ src/main/kotlin/Log.kt
@@ -0,0 +1,5 @@
+package org.iesra
+import java.time.LocalDate
+
+class Log(val dateTime: LocalDate, val level: LogLevel, val message: String) {
+}
--- src/main/kotlin/LogProcessor.kt
+++ src/main/kotlin/LogProcessor.kt
@@ -1,16 +1,19 @@
 package org.iesra
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class LogProcessor {
     fun process(logs: Array<String>) {
     logs.map {
         log ->
-        val bracketsInfo = log.substringBefore("]").split(" ")
+        val bracketsInfo = log.substringBefore("]").substringAfter("[")
         val afterBracketsInfo = log.substringAfter("]").split(" ")
-        val day = bracketsInfo[0]
-        val date = bracketsInfo[1]
-        val status = afterBracketsInfo[0]
-        val message = afterBracketsInfo.joinToString( " ")
-        log(day,date,status,message)
+        val level = LogLevel.valueOf(afterBracketsInfo[0])
+        val message = afterBracketsInfo.drop(1).joinToString( " ")
+        val formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd hh:mm:ss")
+        val dateTime = LocalDate.parse(bracketsInfo,formatter)
+        Log(dateTime,level,message)
     }
     }
 }
