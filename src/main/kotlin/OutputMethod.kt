5652baf1d54c12c8a471e6a84952ef463ac4e489
alopgau <alopezg@ieslabahia.es>
2026-04-08 20:30:35 +0200

    Output method corregido, niveles disponibles en log stats y consoleOutput hecho

--- src/main/kotlin/OutputMethod.kt
+++ src/main/kotlin/OutputMethod.kt
@@ -1,5 +1,18 @@
 package org.iesra
 
+import java.time.LocalDateTime
+
 interface OutputMethod {
-    fun showOutput()
+    fun showOutput(
+        fileName: String,
+        fromDate: String,
+        toDate: String,
+        levelsIncluded: List<LogLevel>,
+        processedLines: Int,
+        validLines: Int,
+        invalidLines: Int,
+        levelsCounted: Map<LogLevel, Int>,
+        firstDate: LocalDateTime,
+        lastDate: LocalDateTime
+    ): String
 }
