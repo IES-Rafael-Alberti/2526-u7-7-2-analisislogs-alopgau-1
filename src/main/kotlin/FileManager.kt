88dbbd503e4465f29791ecf6e4349ca689b97ed3
alopgau <alopezg@iesrafaelalberti.es>
2026-04-08 14:12:56 +0200

    FileManager

--- src/main/kotlin/FileManager.kt
+++ src/main/kotlin/FileManager.kt
@@ -1,4 +1,17 @@
 package org.iesra
+import java.io.File
+class FileManager(path: String) {
+    fun process(path: String): List<String> {
+        if (pathIsValid(path)) {
+            val f = File(path)
+            return read(f)
+        }
+        return emptyList()
+    }
+    fun read(file: File) = file.readLines()
+    fun pathIsValid(path: String): Boolean {
+        val f = File(path)
+    return f.exists() && f.isFile && f.canWrite()
+    }
 
-class FileManager {
 }
