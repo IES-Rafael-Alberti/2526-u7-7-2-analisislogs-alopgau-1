bcf29d5aa9c87e3ea4273f1699ea53488077f495
alopgau <alopezg@ieslabahia.es>
2026-04-08 19:48:01 +0200

    Log Stats hecho

--- src/main/kotlin/LogStats.kt
+++ src/main/kotlin/LogStats.kt
@@ -1,4 +1,10 @@
 package org.iesra
 
-class LogStats {
+class LogStats(invalidLines: List<String>, logs: List<Log>) {
+    fun countValid(logs: List<Log>) = logs.size
+    fun countInvalid(invalidLines: List<String>) = invalidLines.size
+    fun countByLevel(logs: List<Log>) = logs.groupBy { it.level }.mapValues {it.value.size}
+    fun lastDate(logs: List<Log>) = logs.maxBy { it.dateTime }
+    fun firstDate(logs: List<Log>) = logs.minBy { it.dateTime }
+
 }
