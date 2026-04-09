edfd42338ab3c90d5bfc595746ca1d716b7c567d
alopgau <alopezg@iesrafaelalberti.es>
2026-04-09 13:53:24 +0200

    Configurator empezado

--- src/main/kotlin/Configurator.kt
+++ src/main/kotlin/Configurator.kt
@@ -1,10 +1,10 @@
 package org.iesra
 
-class Configurator(val args: Array<String>) {
-    var ignoreInvalid = false
-    var outputMethod: OutputMethod? = null
-
-
-
-
+class Configurator(private val params: Map<String, ConfigStatus>, private val configValues: Map<String, Array<String?>> ) {
+    fun configure() {
+        val activeValues = configValues.values.filterNotNull()
+        params.entries.forEach { (param, status) ->
+            if (param in configValues.keys)
+        }
+    }
 }
