48b26ea35491cf30386df7bfeff1222ed30d79b6
alopgau <alopezg@ieslabahia.es>
2026-04-09 17:31:08 +0200

    Configurator hecho

--- src/main/kotlin/Configurator.kt
+++ src/main/kotlin/Configurator.kt
@@ -1,10 +1,17 @@
 package org.iesra
 
 class Configurator(private val params: Map<String, ConfigStatus>, private val configValues: Map<String, Array<String?>> ) {
+
     fun configure() {
-        val activeValues = configValues.values.filterNotNull()
-        params.entries.forEach { (param, status) ->
-            if (param in configValues.keys)
+        val finalConfig = params.map {
+        var booleanStatus = false
+            var argValues: List<String>? = null
+            if (it.value == ConfigStatus.ACTIVE) {
+                booleanStatus = true
+            }
+            if (it.key in configValues.keys) argValues = configValues[it.key]?.filterNotNull()
+
+            mapOf<String, Pair<Boolean, List<String>?>>(it.key to Pair(booleanStatus, argValues))
         }
     }
 }
